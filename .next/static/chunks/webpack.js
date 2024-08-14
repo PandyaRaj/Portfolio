ebd8b337b8337ea0c158f59cdc7dbbbcc8eda9a3
Raj Pandya <rajpandya018@gmail.com>
2024-08-14 17:59:43 -0400

    Img updated

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0b2cb33eb9e67949"; }
+/******/ 		__webpack_require__.h = function() { return "940bbd790b645318"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
