1c760db14becca62097ce67f227bc079364ed486
Raj Pandya <rajpandya018@gmail.com>
2024-08-15 17:32:18 -0400

    Resume updated

--- components/Common/Intro/Download.jsx
+++ components/Common/Intro/Download.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 const Download = ({ icon }) => {
   return (
     <>
-      <Link href='https://drive.google.com/file/d/1flxQMoAzaY6xx4e_w8yLJuGrLrf2SBkK/view?usp=sharing' target='_blank' rel='noopener noreferrer' className='flex flex-row text-LightGray items-center gap-x-4 pb-14 pt-4'>
+      <Link href='https://drive.google.com/file/d/1ysUtbW_hurSWE2IuDbkIb0-tW6Sh8ouU/view?usp=sharing' target='_blank' rel='noopener noreferrer' className='flex flex-row text-LightGray items-center gap-x-4 pb-14 pt-4'>
         <span className='text-Snow'>Download Resume</span>
         <span>{icon}</span>
       </Link>
